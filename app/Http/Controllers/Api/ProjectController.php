e1992475bfaee5036943b2a8f0a244c8e98d885d
Federico-Maiocchi <maiocchifederico@gmail.com>
2024-01-15 14:21:51 +0100

    ADD: route api in api.php

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -10,6 +10,11 @@ class ProjectController extends Controller
 {
     public function index() {
 
-        
+        $projects = Project::all();
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects
+        ]);
     }
 }
